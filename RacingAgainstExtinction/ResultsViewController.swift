7358e75f2b511d25d1ac9ebbd839240fbc5eb758
PathwaysToTech <mindy@edfarm.org>
2022-07-28 10:22:31 -0500

    Initial Commit

---
+++ RacingAgainstExtinction/ResultsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResultsViewController.swift
+//  RacingAgainstExtinction
+//
+//  Created by Student on 7/27/22.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RacingAgainstExtinction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RacingAgainstExtinction
+//
+//  Created by Student on 7/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
